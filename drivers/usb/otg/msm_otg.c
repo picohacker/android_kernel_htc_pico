bf0ad29e9bd2d0a06cf92a44e41479b5ecfb2cfb
vaibhav121 <bhasinvaibhav97@gmail.com>
2014-01-11 21:56:43 +0530

    Some random features :p

--- drivers/usb/otg/msm_otg.c
+++ drivers/usb/otg/msm_otg.c
@@ -43,6 +43,11 @@
 #include <mach/clk.h>
 #include <mach/cable_detect.h>
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastchg.h>
+#define USB_FASTCHG_LOAD 1000 /* uA */
+#endif
+
 #define MSM_USB_BASE	(motg->regs)
 #define DRIVER_NAME	"msm_otg"
 
@@ -1016,6 +1021,15 @@ static void msm_otg_notify_charger(struct msm_otg *motg, unsigned mA)
 
 	if (motg->cur_power == mA)
 		return;
+
+#ifdef CONFIG_FORCE_FAST_CHARGE
+  if (force_fast_charge == 1) {
+      mA = USB_FASTCHG_LOAD;
+      pr_info("USB fast charging is ON - 1000mA.\n");
+  } else {
+    pr_info("USB fast charging is OFF.\n");
+  }
+#endif
 
 	USBH_INFO("Avail curr from USB = %u\n", mA);
 	pm8921_charger_vbus_draw(mA);
